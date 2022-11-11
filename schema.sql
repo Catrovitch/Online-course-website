6d8829ff288b010dcc53234c5f3e69ed637f0881
Alexander <ale.engelhardt@outlook.com>
2022-11-11 11:54:35 +0200

    added schema.sql, fly.toml, Procfile

---
+++ schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE Courses (id SERIAL PRIMARY KEY, name TEXT, course_id INTEGER, description TEXT);
+CREATE TABLE USERS (id SERIAL PRIMARY KEY, username TEXT, password TEXT, STATUS BOOLEAN);
