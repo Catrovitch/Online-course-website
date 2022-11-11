89370baeda87d39f6e5afb4e2c9c1efc9b0c8caa
Alexander <ale.engelhardt@outlook.com>
2022-11-11 15:48:31 +0200

    first working

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
 from flask import Flask
-from flask import redirect, render_template, request, session
 from os import getenv
-from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
 app.secret_key = getenv("SECRET_KEY")
-db = SQLAlchemy(app)
 
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/login",methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    # TODO: check username and password
-    session["username"] = username
-    return redirect("/")
-
-@app.route("/logout")
-def logout():
-    del session["username"]
-    return redirect("/")
+import routes
