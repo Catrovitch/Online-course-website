50629bb0a9a43316cb0f948b185e4a7ce47b4865
Alexander <ale.engelhardt@outlook.com>
2022-11-11 14:19:47 +0200

    Fly.io now working

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from flask import Flask
-from flask import redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import redirect, render_template, request, session
 from os import getenv
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+app.secret_key = getenv("SECRET_KEY")
 db = SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
-    db.session.execute("INSERT INTO visitors (time) VALUES (NOW())")
-    db.session.commit()
-    result = db.session.execute("SELECT COUNT(*) FROM visitors")
-    counter = result.fetchone()[0]
-    return render_template("index.html", counter=counter) 
+    return render_template("index.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    # TODO: check username and password
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
